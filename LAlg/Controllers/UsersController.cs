0c86fb95388bec6f87f5f05a74e616fac63165a6
Your Name <you@example.com>
2020-07-04 17:11:44 +0300

    AIT-WOPPI-1 Добавить парам авторизации. Добавлена проверка авторизации

--- LAlg/Controllers/UsersController.cs
+++ LAlg/Controllers/UsersController.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BotAppData;
+using BotAppData.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using BotAppData;
-using BotAppData.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace LAlg.Controllers
 {
+    [Authorize]
     public class UsersController : Controller
     {
         private readonly BotAppContext _context;
