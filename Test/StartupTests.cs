2bc850f85ec6254fe83e690c759917378b3d39c3
sergfomin2011 <sergfomin2011@gmail.com>
2020-07-15 16:03:38 +0300

    Изменил проект Test, убрал nunit там где он не нужен, создал пустые проверки StartupTest ProgramTests

---
+++ Test/StartupTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using LAlg;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.TestHost;
+using System.Net.Http;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace LAlg.Tests
+{
+    [TestFixture()]
+    public class StartupTests
+    {
+        private TestServer _server;
+        private HttpClient _client;
+        [SetUp]
+        public void Init()
+        {
+            _server = new TestServer(new WebHostBuilder()
+                       .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        [Test()]
+        public void StartupTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void ConfigureServicesTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void ConfigureTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
